febf57bb22d8fe8fbdd7390d8f1a2311f94be1fc
Aditya Sarapure <adityasarapure100@gmail.com>
2026-02-18 11:17:52 +0530

    Added variables and data types basics

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# Python
+
+## 1. Variables and Data Types
+
+In this section I learned:
+- int
+- float
+- string
+- boolean
+- type() function
+- type conversion
+
+This repository documents my journey of Intership
---
+++ Variables_Datatypes.py
@@ -0,0 +1,29 @@
+# Variables and Data Types
+
+# Integer
+age = 21
+
+# Float
+height = 5.8
+
+# String
+name = "Aditya"
+
+# Boolean
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Type Checking
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+# Type Conversion
+num = "50"
+converted_num = int(num)
+print(converted_num + 10)
